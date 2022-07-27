987f71f48dd45c3118c24553d006f1ed40302d85
bickkysahani <bickkysahani@gmail.com>
2022-07-28 00:42:58 +0545

    Added user profile feed to API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,7 +9,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-
+from rest_framework.permissions import IsAuthenticated
 
 class HelloApiView(APIView):
     """Test Api View"""
@@ -91,4 +91,18 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus,IsAuthenticated)
+
+    def perform_create(self,serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
+
+    def get_queryset(self):
+        """Return the last 5 feed items for the user"""
+        return self.queryset.filter(user_profile=self.request.user)
